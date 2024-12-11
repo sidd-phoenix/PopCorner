49e04e05c252d5661161ab6f217810761159eef3
Phoenix <aadilsiddiqui2016+github@gmail.com>
2024-12-11 17:36:34 +0530

    Docker-compose command working

--- api-gateway/gateway.js
+++ api-gateway/gateway.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }));
 
 // Routes to respective services
-app.all('/users/*', proxy('http://localhost:3002'));
-app.all('/bookings/*', proxy('http://localhost:3003'));
-app.all('/movies/*', proxy('http://localhost:3004'));
-app.all('/snacks/*', proxy('http://localhost:3005'));
-app.all('/payments/*', proxy('http://localhost:3006'));
-app.all('/theaters/*', proxy('http://localhost:3007'));
+app.all('/users/*', proxy('http://user-service:3002'));
+app.all('/bookings/*', proxy('http://booking-service:3003'));
+app.all('/movies/*', proxy('http://movie-service:3004'));
+app.all('/snacks/*', proxy('http://snack-service:3005'));
+app.all('/payments/*', proxy('http://payment-service:3006'));
+app.all('/theaters/*', proxy('http://theater-service:3007'));
 
 app.listen(3001, () => console.log('API Gateway running on port 3001'));
